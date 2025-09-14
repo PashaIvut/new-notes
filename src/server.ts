ce096d94ecadacd902962d28e20ee66c9f89467b
Павел Ивуть <ivutjob@yandex.ru>
2025-09-14 16:53:22 +0300

    refactor: add console.log in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import { createYoga, createSchema } from 'graphql-yoga'
 import { createServer } from 'http'
+import { config } from './config'
 import { typeDefs } from './schema/typeDefs.generated'
 import { resolvers } from './schema/resolvers.generated'
  
 const yoga = createYoga({ schema: createSchema({ typeDefs, resolvers }) })
 const server = createServer(yoga)
-server.listen(4000)
+server.listen(config.port, () => {
+    console.log(`GraphQL сервер запущен на http://localhost:${config.port}/graphql`)
+})
